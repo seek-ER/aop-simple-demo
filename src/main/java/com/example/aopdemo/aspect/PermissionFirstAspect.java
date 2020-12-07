7839559a5b7dd7595698f78d9e03f2b2c8e84249
Anson <1072476988@qq.com>
2020-12-07 14:56:16 +0800

    refactor

--- src/main/java/com/example/aopdemo/aspect/PermissionFirstAspect.java
+++ src/main/java/com/example/aopdemo/aspect/PermissionFirstAspect.java
@@ -1,4 +1,4 @@
-package com.example.aopdemo.advice;
+package com.example.aopdemo.aspect;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -6,13 +6,16 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 @Order(1)
-public class PermissionFirstAdvice {
+public class PermissionFirstAspect {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     // 定义一个切面，括号内写入第1步中自定义注解的路径
     @Pointcut("@annotation(com.example.aopdemo.annotation.PermissionAnnotation)")
@@ -21,14 +24,14 @@ public class PermissionFirstAdvice {
 
     @Around("permissionCheck()")
     public Object permissionCheckFirst(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("===================第一个切面===================：" + System.currentTimeMillis());
+        logger.info("===================第一个切面===================：" + System.currentTimeMillis());
 
         //获取请求参数，详见接口类
         Object[] objects = joinPoint.getArgs();
         Long id = ((JSONObject) objects[0]).getLong("id");
         String name = ((JSONObject) objects[0]).getString("name");
-        System.out.println("id1->>>>>>>>>>>>>>>>>>>>>>" + id);
-        System.out.println("name1->>>>>>>>>>>>>>>>>>>>>>" + name);
+        logger.info("id1->>>>>>>>>>>>>>>>>>>>>>" + id);
+        logger.info("name1->>>>>>>>>>>>>>>>>>>>>>" + name);
 
         // id小于0则抛出非法id的异常
         if (id < 0) {
